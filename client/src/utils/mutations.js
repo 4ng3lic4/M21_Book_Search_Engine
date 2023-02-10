f1cd098a4ae496eab85a2728e5458c9deeb24d39
4ng3lic4 <ositosviajeros@hotmail.com>
2023-02-10 12:26:41 -0500

    debugging mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,7 +17,7 @@ mutation login($email: String!,$password: String!) {
 //ADD_USER
 export const ADD_USER = gql `
 mutation addUser($username: String!, $email: String!,$password: String!) {
-    login(email: $email, password: $password){
+    addUser(username: $username, email: $email, password: $password){
         token
         user {
             _id
