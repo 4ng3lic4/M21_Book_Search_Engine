2dbd53b5fe287ae8100f0f82abdaa782246bdfb1
4ng3lic4 <ositosviajeros@hotmail.com>
2023-02-09 21:55:34 -0500

    adding imports and updating queries

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -17,7 +17,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  //loginUseer
+  //loginUser
   const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,40 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { useQuery, useMutation} from '@apollo/react-hooks';
+import { GET_ME} from '../utils/queries';
+import {REMOVE_BOOK} from '..utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
-
+  const { loading, data } = useQuery (GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  if(!userData?.username){
+    return (
+      <h4>
+        Please login </h4>
+    );
+  }
 
-        if (!token) {
-          return false;
-        }
+ 
 
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -45,14 +32,17 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+       await deleteBook({
+        variables: {bookId: bookId},
+        update: cache => {
+          const data = cache.readQuery({ query: GET_ME});
+          const userDataCache = data.me;
+          const savedBooksCache = userDataCache.filter((book) => book.bookId !== bookId ); 
+          data.me.savedBooks = updateBookCache;
+          cache.writeQuery({ query : GET_ME, data: {data: {...data.me.savedBooks}}})
+        }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -61,7 +51,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
